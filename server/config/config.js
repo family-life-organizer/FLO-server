3e1705a0b1fa027e1f6e4af1ef7a6fa0a31d3746
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-01 20:43:00 +0100

    refactored config

--- server/config/config.js
+++ server/config/config.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-      url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
     dialect: "postgres"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "family_organize",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    url: process.env.DATABASE_URL,
+    dialect: "postgres"
   },
   production: {
     url: process.env.DATABASE_URL,
