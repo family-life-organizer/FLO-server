1ef453d91047c50fb1cd4dad7124b16e0dba2a3a
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-01 21:58:44 +0100

    signup functionalty

--- server/models/user.js
+++ server/models/user.js
@@ -21,3 +21,29 @@ module.exports = (sequelize, DataTypes) => {
   };
   return User;
 };
+
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     firstName: {
+//       type: DataTypes.STRING, 
+//       allowNull: false
+//     },
+//     lastName: DataTypes.STRING,
+//     email: {
+//       type: DataTypes.STRING,
+//       unique: true,
+//     },
+//     username: {
+//       type: DataTypes.STRING,
+//       unique: true,
+//     },
+//     isAdmin: DataTypes.BOOLEAN,
+//   }, {});
+//   User.associate = (models) => {
+//     User.BelongsTo(models.Family, {
+//       foreignKey: 'userId',
+//       as: 'family',
+//     });
+//   };
+//   return User;
+// };
--- server/router/index.js
+++ server/router/index.js
@@ -7,10 +7,11 @@ router.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to the Family_org API!',
 }));
 
-
-router.get('/signup', (req, res) => {
-  
+router.get("/signup", (req, res) => {
+  User.findOne({ where: { email: "ezekiel" } })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  return res.json("great");
 });
-
 
 export default router;
