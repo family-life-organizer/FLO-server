cf788399d199b3e90291e65c9bc11b2a1588edc4
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-01 22:00:01 +0100

    signup functionalty

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,14 @@
-import express from 'express';
-
+import express from "express";
+import models from "../models";
 const router = express.Router();
 
+const { User } = models;
 
-router.get('/', (req, res) => res.status(200).send({
-  message: 'Welcome to the Family_org API!',
-}));
+router.get("/", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to the Family_org API!"
+  })
+);
 
 router.get("/signup", (req, res) => {
   User.findOne({ where: { email: "ezekiel" } })
