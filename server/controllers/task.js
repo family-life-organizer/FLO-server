bc9a54ee11ece3613842727e40a5441d512dac38
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-02 20:01:16 +0100

    update task

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -40,4 +40,91 @@ export default class TaskController {
       });
     }
   }
+
+  static async updateTask(req, res) {
+    let { userId, description, password, lastName, firstName, email } = req.body;
+    if (!username && !password && !lastName && !firstName && !email) {
+      return res.status(400).json({
+        status: "error",
+        message: "At least one field must be provided for update to happen"
+      });
+    }
+    try {
+      const existingUser = await User.findOne({ where: { id: userId } });
+
+      if (!existingUser) {
+        return res.status(404).json({
+          status: "error",
+          message: "User not found"
+        });
+      }
+
+      username = username || existingUser.username;
+      password = password || existingUser.password;
+      email = email || existingUser.email;
+      firstName = firstName || existingUser.firstName;
+      lastName = lastName || existingUser.lastName;
+
+      const existingUsername = await User.findOne({
+        where: {
+          username,
+          id: {
+            [Op.not]: existingUser.id
+          }
+        }
+      });
+
+      if (existingUsername) {
+        return res.status(409).json({
+          status: "error",
+          message: "Username already existing, Kindly choose another username"
+        });
+      }
+
+      const existingEmail = await User.findOne({
+        where: {
+          email,
+          id: {
+            [Op.not]: existingUser.id
+          }
+        }
+      });
+
+      if (existingEmail) {
+        return res.status(409).json({
+          status: "error",
+          message: "Email already existing, Kindly choose another username"
+        });
+      }
+
+      const hashedPassword = await generateHash(password);
+
+      const updatedUser = await User.update(
+        {
+          username,
+          password: hashedPassword,
+          lastName,
+          firstName,
+          email
+        },
+        { where: { id: userId } }
+      );
+
+      if (updatedUser) {
+        return res.status(200).json({
+          status: "success",
+          message: "User Updated Sucessfully"
+        });
+      }
+
+      return res
+        .status(400)
+        .json({ status: "error", message: "Error updating user" });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "Error updating user" });
+    }
+ 
+  }
 }
--- server/router/index.js
+++ server/router/index.js
@@ -3,7 +3,7 @@ import Users from "../controllers/user";
 import middleware from '../middlewares/authentication'
 import Category from '../controllers/category'
 import Task from '../controllers/task'
-const { addTask } = Task;
+const { addTask, updateTask } = Task;
 const { isAuthenticated, isAdmin } = middleware;
 const { createCategory, getFamilyCategories }  = Category
 const { registerUser, loginUser, addUser, updateProfile, getFamilyMembers, getUserDetails } = Users;
@@ -28,4 +28,5 @@ router.get('/categories/family', isAdmin, getFamilyCategories )
 router.get('/users/family', isAdmin, getFamilyMembers )
 router.get('/users/:userId', isAuthenticated, getUserDetails)
 router.post('/tasks', isAdmin, addTask)
+router.patch('/tasks', isAdmin, updateTask)
 export default router;
